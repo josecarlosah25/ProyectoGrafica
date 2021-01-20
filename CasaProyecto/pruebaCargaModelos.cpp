828926164ceb339044f011d5c39351d0f3e7087c
Carlos Alcántara <carlooz_extrem@hotmailcom>
2021-01-19 19:04:52 -0600

    solo falta 1 animacion y el objeto

--- CasaProyecto/pruebaCargaModelos.cpp
+++ CasaProyecto/pruebaCargaModelos.cpp
@@ -28,6 +28,7 @@ int SCREEN_WIDTH, SCREEN_HEIGHT;
 void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode );
 void MouseCallback( GLFWwindow *window, double xPos, double yPos );
 void DoMovement( );
+void animacionAccidente();
 
 // Camera
 Camera camera( glm::vec3( 0.0f, 0.0f, 3.0f ) );
@@ -37,6 +38,13 @@ bool firstMouse = true;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
+
+//Variables para animaciones
+float movCajon = 0; bool cajonActive = false;
+float movRetrato = 0; bool retratoActive = false;
+float movPuerta = 0; bool puertaActive = false;
+
+float rotAccidenteB, rotAccidenteJ, movAccidente = 0; bool accidenteP1 = false; bool accidenteP2 = false; bool accidenteP3 = false;
 
 int main( )
 {
@@ -96,6 +104,8 @@ int main( )
     Model retrato((char*)"Models/retrato/retrato.obj");
     Model silla((char*)"Models/silla/silla.obj");
     Model puertaPrin((char*)"Models/door/puerta.obj");
+    Model banquito((char*)"Models/banco/banco.obj");
+    Model jarron((char*)"Models/jarron/jarron.obj");
    
 
     // Draw in wireframe
@@ -114,6 +124,7 @@ int main( )
         // Check and call events
         glfwPollEvents( );
         DoMovement( );
+        animacionAccidente();
         
         // Clear the colorbuffer
         glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
@@ -142,7 +153,7 @@ int main( )
         
         // Draw the loaded model
         model= glm::mat4(1);
-        model = glm::translate(model, glm::vec3(0.0f, tras, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        model = glm::translate(model, glm::vec3(0.0f, tras, 0.0f-movCajon)); // Translate it down a bit so it's at the center of the scene
         //model = glm::scale( model, glm::vec3( 0.02f, 0.02f, 0.02f ) );	// It's a bit too big for our scene, so scale it down
         //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -162,7 +173,7 @@ int main( )
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(0.417576f, 4.52347f + tras, -1.28148f)); // Translate it down a bit so it's at the center of the scene
         //model = glm::scale( model, glm::vec3( 0.02f, 0.02f, 0.02f ) );	// It's a bit too big for our scene, so scale it down
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(movRetrato), glm::vec3(1.0f, 0.0f, 0.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
         retrato.Draw(shader);
@@ -180,10 +191,29 @@ int main( )
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(-0.67457f, 1.05998f + tras, -0.914868f)); // Translate it down a bit so it's at the center of the scene
         //model = glm::scale( model, glm::vec3( 0.02f, 0.02f, 0.02f ) );	// It's a bit too big for our scene, so scale it down
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(movPuerta), glm::vec3(0.0f, 1.0f, 0.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
         puertaPrin.Draw(shader);
+
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-0.319778f-movAccidente, 3.90331f + tras, -4.38809f)); // Translate it down a bit so it's at the center of the scene
+        //model = glm::scale( model, glm::vec3( 0.02f, 0.02f, 0.02f ) );	// It's a bit too big for our scene, so scale it down
+        model = glm::rotate(model, glm::radians(rotAccidenteJ), glm::vec3(0.0f, 0.0f, 1.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        jarron.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-0.319778f, 3.90331f + tras, -4.38809f)); // Translate it down a bit so it's at the center of the scene
+        //model = glm::scale( model, glm::vec3( 0.02f, 0.02f, 0.02f ) );	// It's a bit too big for our scene, so scale it down
+        model = glm::rotate(model, glm::radians(rotAccidenteB), glm::vec3(0.0f, 0.0f, 1.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        banquito.Draw(shader);
+
+
         
         // Swap the buffers
         glfwSwapBuffers( window );
@@ -191,6 +221,43 @@ int main( )
     
     glfwTerminate( );
     return 0;
+}
+
+void reset() {
+    cajonActive = false;
+    movCajon = 0;
+    retratoActive = false;
+    movRetrato = 0;
+    puertaActive = false;
+    movPuerta = 0;
+
+}
+
+void animacionAccidente() {
+    float vel=3;
+    if (accidenteP1) {
+        rotAccidenteB += vel;
+        rotAccidenteJ += vel;
+        if (rotAccidenteB >= 45) {
+            accidenteP1 = false;
+            accidenteP2 = true;
+        }
+    }
+    if (accidenteP2) {
+        rotAccidenteB += vel;
+        rotAccidenteJ += vel;
+        movAccidente += vel / 100;
+        if (rotAccidenteB >= 90) {
+            accidenteP2 = false;
+            accidenteP3 = true;
+        }
+    }
+    if (accidenteP3) {
+        rotAccidenteJ += vel;
+        if (rotAccidenteJ >= 95.5) {
+            accidenteP3 = false;
+        }
+    }
 }
 
 // Moves/alters the camera positions based on user input
@@ -216,6 +283,66 @@ void DoMovement( )
     {
         camera.ProcessKeyboard( RIGHT, deltaTime );
     }
+
+    if (keys[GLFW_KEY_R]) {
+        reset();
+    }
+
+    if (keys[GLFW_KEY_C]) {
+        cajonActive = true;
+        if (movCajon >= 0.3) {
+            movCajon = 0;
+        }
+    }
+
+    if (cajonActive) {
+        if (movCajon < 0.3) {
+            movCajon += 0.005;
+        }
+        else {
+            cajonActive = false;
+        }
+    }
+
+    if (keys[GLFW_KEY_G]) {
+        retratoActive = true;
+        if (movRetrato <= -110) {
+            movRetrato = 0;
+        }
+    }
+
+    if (retratoActive) {
+        if (movRetrato > -110) {
+            movRetrato -= 5;
+        }
+        else {
+            retratoActive = false;
+        }
+    }
+
+    if (keys[GLFW_KEY_V]) {
+        puertaActive = true;
+        if (movPuerta <= -90) {
+            movPuerta = 0;
+        }
+    }
+
+    if (puertaActive) {
+        if (movPuerta > -90) {
+            movPuerta -= 5;
+        }
+        else {
+            puertaActive = false;
+        }
+    }
+
+    if (keys[GLFW_KEY_F]) {
+        rotAccidenteB = 0;
+        rotAccidenteJ = 0;
+        movAccidente = 0;
+        accidenteP1 = true;
+    }
+
 }
 
 // Is called whenever a key is pressed/released via GLFW
